38f76f984d88dabfea12793cf77d8cd3c94ce103
Nicholas <nicholaspriegel@gmail.com>
2024-04-01 14:36:48 -0400

    added functions to readme

--- app.js
+++ app.js
@@ -20,22 +20,20 @@ let userSym2 = ""
 
 
 const userSymCall = () => {                     // this is the function to let users pick their symbols 
+
     userSym1 = prompt(`What is your Symbol ${user1}? X or O? `);
-        if (userSym1 === "X" || userSym1 === "x" ) {  // checks to see if user inputed lower or uppercase X
-            userSym1 = "X"
-            userSym2 = "O"
-            console.log(`${user1} is ${userSym1} and ${user2} is ${userSym2}` )
-        } else if ( userSym1 === "O" ||  userSym1 === "o" ) {  // checks to see if user inputed lower or uppercase O
-            userSym1 = "O"
-            userSym2 = "X"
-            console.log(`${user1} is ${userSym1} and ${user2} is ${userSym2}`  )
-        } else {
-            console.log(" You did not put in a valid response");
-        }
-
     
-
-
+    if (userSym1 === "X" || userSym1 === "x" ) {  // checks to see if user inputed lower or uppercase X
+        userSym1 = "X"
+        userSym2 = "O"
+        console.log(`${user1} is ${userSym1} and ${user2} is ${userSym2}` )
+    } else if ( userSym1 === "O" ||  userSym1 === "o" ) {  // checks to see if user inputed lower or uppercase O
+        userSym1 = "O"
+        userSym2 = "X"
+        console.log(`${user1} is ${userSym1} and ${user2} is ${userSym2}`  )
+    } else {
+        console.log(" You did not put in a valid response");
+    }
 }
 
 userSymCall()
