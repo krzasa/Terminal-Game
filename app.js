4a148f177148822dafdaf568eced7a4fe119c5f0
Nicholas <nicholaspriegel@gmail.com>
2024-04-01 15:31:55 -0400

    added gameboard and picksquare features

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ const prompt = require('prompt-sync')();
 let user1 = ""
 let user2 = ""
 
-const userCall = () => {    // creating a function for asking the name of the players 
- user1 = prompt('What is your name Player 1? ');          // create variable for player 1 from user prompt
-console.log(`Your name is ${user1}`);
-
-
-  user2 = prompt('What is your name Player 2? ');  // create variable for player 2 prompt 
-console.log(`Your name is ${user2}`);
-
-}
-
-userCall()
- 
 let userSym1 = ""
 let userSym2 = ""
 
+const userCall = () => {  // creating a function for asking the name of the players 
+    
+    user1 = prompt('What is your name Player 1? '); // create variable for player 1 from user prompt
+    console.log(`Your name is ${user1}`);
 
-const userSymCall = () => {                     // this is the function to let users pick their symbols 
+    user2 = prompt('What is your name Player 2? ');  // create variable for player 2 prompt 
+    console.log(`Your name is ${user2}`);
+
+}
+
+// userCall() 
+
+
+const userSymCall = () => { // this is the function to let users pick their symbols 
 
     userSym1 = prompt(`What is your Symbol ${user1}? X or O? `);
-    
+
     if (userSym1 === "X" || userSym1 === "x" ) {  // checks to see if user inputed lower or uppercase X
         userSym1 = "X"
         userSym2 = "O"
@@ -36,13 +36,35 @@ const userSymCall = () => {                     // this is the function to let u
     }
 }
 
-userSymCall()
+// userSymCall()
 
-// ---
-// ---
-// ---
+const topRow = [1, 2, 3]
+const midRow = [4, 5, 6]
+const bottomRow = [7, 8, 9]
 
-// x--
-// -o-
-// --x
+// This shows the gameboard
+const showGameBoard = () => {
+    console.log(topRow);
+    console.log(midRow);
+    console.log(bottomRow);
+}
 
+showGameBoard()
+
+// This is the feature for picking the square
+const pickSquare = () => {
+    let choice 
+    choice = prompt("Please pick a square by entering a number from 1 to 9: ")
+    if (choice >= 1 && choice <=3){
+        topRow[choice - 1] = 'X'
+    }
+    if (choice >= 4 && choice <= 6){
+        midRow[choice - 1] = 'X'
+    }
+    if (choice >= 7 && choice <= 9){
+        bottomRow[choice - 1] = 'X'
+    }
+    showGameBoard()
+}
+
+pickSquare()
